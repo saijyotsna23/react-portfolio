be9a0d0ead20e5e3c874946859a7320ba1f6c506
Jyotsna <svykuntam@albany.edu>
2024-03-04 17:11:40 -0500

    media_final2

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -53,45 +53,46 @@ const ContactMeSection = () => {
   });
 
   return (
-    <FullScreenSection isDarkBackground backgroundColor="#512DA8" py={16} spacing={8}>
-      <VStack w="100%" p={8} alignItems="center">
-        <Heading as="h1" id="contactme-section" mb={4}>
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="90%" maxW="500px">
-          <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={4} alignItems="stretch">
-              <FormControl isInvalid={formik.errors.firstName && formik.touched.firstName}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input {...formik.getFieldProps("firstName")} id="firstName" name="firstName" size="lg" />
-                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={formik.errors.email && formik.touched.email}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input {...formik.getFieldProps("email")} id="email" name="email" type="email" size="lg" />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select {...formik.getFieldProps("type")} id="type" name="type" size="lg">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">Job Offer</option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={formik.errors.comment && formik.touched.comment}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea {...formik.getFieldProps("comment")} id="comment" name="comment" size="lg" />
-                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" isLoading={isLoading} size="lg">
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
+    <FullScreenSection isDarkBackground backgroundColor="#512DA8" py={{ base: 4, md: 16 }} spacing={8}>
+  <VStack w={{ base: "100%", md: "1024px" }} px={{ base: 4, md: 32 }} py={{ base: 2, md: 32 }} alignItems="center">
+    <Heading as="h1" id="contactme-section" size="xl" textAlign="center" mb={4}>
+      Contact me
+    </Heading>
+    <Box p={{ base: 4, md: 6 }} rounded="md" w={{ base: "100%", md: "80%" }} alignSelf="center">
+      <form onSubmit={formik.handleSubmit}>
+        <VStack spacing={4} alignItems="flex-start" w="100%">
+          <FormControl isInvalid={formik.errors.firstName && formik.touched.firstName} w="100%">
+            <FormLabel htmlFor="firstName">Name</FormLabel>
+            <Input {...formik.getFieldProps("firstName")} id="firstName" name="firstName" />
+            <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={formik.errors.email && formik.touched.email} w="100%">
+            <FormLabel htmlFor="email">Email Address</FormLabel>
+            <Input {...formik.getFieldProps("email")} id="email" name="email" type="email" />
+            <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+          </FormControl>
+          <FormControl w="100%">
+            <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+            <Select {...formik.getFieldProps("type")} id="type" name="type">
+              <option value="hireMe">Freelance project proposal</option>
+              <option value="openSource">Job Offer</option>
+              <option value="other">Other</option>
+            </Select>
+          </FormControl>
+          <FormControl isInvalid={formik.errors.comment && formik.touched.comment} w="100%">
+            <FormLabel htmlFor="comment">Your message</FormLabel>
+            <Textarea {...formik.getFieldProps("comment")} id="comment" name="comment" height={250} />
+            <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+          </FormControl>
+          <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+            Submit
+          </Button>
+        </VStack>
+      </form>
+    </Box>
+  </VStack>
+</FullScreenSection>
+
   );
 };
 
