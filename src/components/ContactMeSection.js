9ee4d6593541677555e19bc3e85360421f6a07cf
Jyotsna <svykuntam@albany.edu>
2024-03-04 15:19:42 -0500

    media

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -47,50 +47,51 @@ const ContactMeSection = () => {
         }
       } catch (error) {
         console.error("An error occurred while submitting the form:", error);
-        onOpen("error", "An error occurred while submitting the form. Please try again later.This is just to show case my skill");
+        onOpen("error", "An error occurred while submitting the form. Please try again later.This is just to show case my");
       }
     },
   });
 
   return (
     <FullScreenSection isDarkBackground backgroundColor="#512DA8" py={16} spacing={8}>
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={4}>
-              <FormControl isInvalid={formik.errors.firstName && formik.touched.firstName}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input {...formik.getFieldProps("firstName")} id="firstName" name="firstName" />
-                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={formik.errors.email && formik.touched.email}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input {...formik.getFieldProps("email")} id="email" name="email" type="email" />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select {...formik.getFieldProps("type")} id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">Job Offer</option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={formik.errors.comment && formik.touched.comment}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea {...formik.getFieldProps("comment")} id="comment" name="comment" height={250} />
-                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
+      <VStack w={{ base: "100%", md: "1024px" }} p={32} alignItems="flex-start">
+  <Heading as="h1" id="contactme-section">
+    Contact me
+  </Heading>
+  <Box p={6} rounded="md" w="100%">
+    <form onSubmit={formik.handleSubmit}>
+      <VStack spacing={4}>
+        <FormControl isInvalid={formik.errors.firstName && formik.touched.firstName}>
+          <FormLabel htmlFor="firstName">Name</FormLabel>
+          <Input {...formik.getFieldProps("firstName")} id="firstName" name="firstName" />
+          <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={formik.errors.email && formik.touched.email}>
+          <FormLabel htmlFor="email">Email Address</FormLabel>
+          <Input {...formik.getFieldProps("email")} id="email" name="email" type="email" />
+          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+          <Select {...formik.getFieldProps("type")} id="type" name="type">
+            <option value="hireMe">Freelance project proposal</option>
+            <option value="openSource">Job Offer</option>
+            <option value="other">Other</option>
+          </Select>
+        </FormControl>
+        <FormControl isInvalid={formik.errors.comment && formik.touched.comment}>
+          <FormLabel htmlFor="comment">Your message</FormLabel>
+          <Textarea {...formik.getFieldProps("comment")} id="comment" name="comment" height={250} />
+          <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+        </FormControl>
+        <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+          Submit
+        </Button>
       </VStack>
+    </form>
+  </Box>
+</VStack>
+
     </FullScreenSection>
   );
 };
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,4 +1,4 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import Card from "./Card";
 import FullScreenSection from "./FullScreenSection";
@@ -9,24 +9,22 @@ const projects = [
     description:
       "'Little Lemon' is a React-based frontend application designed to streamline table reservations at a restaurant. Users can effortlessly reserve tables through an intuitive interface, enhancing their dining experience.",
     getImageSrc: () => require("../images/ab.jpg"),
-    githubLink:"https://github.com/saijyotsna23/little__lemon.git"
-    
+    githubLink: "https://github.com/saijyotsna23/little__lemon.git"
   },
   {
     title: "warehouse management system",
     description:
       "The aim of this project is to develop and design a comprehensive warehouse management system that will optimize the entire sales and distribution process in a company. Overall, our main goal is to optimize the daily operations and improve the overall business performance of the warehouse companies.",
     getImageSrc: () => require("../images/photo2.jpg"),
-    githubLink:"https://github.com/saijyotsna23/Warehouse-Management-System-Development-.git"
+    githubLink: "https://github.com/saijyotsna23/Warehouse-Management-System-Development-.git"
   },
   {
     title: "Simon Game",
     description:
       "The Simon game created using HTML, CSS, and JavaScript presents players with a sequence of blinking colors. If the player clicks the colors in the correct sequence, they advance to the next level, where the sequence becomes longer. If they make a mistake, the game ends.",
     getImageSrc: () => require("../images/small_game.jpeg"),
-    githubLink:"https://github.com/saijyotsna23/Simon-Game.git"
+    githubLink: "https://github.com/saijyotsna23/Simon-Game.git"
   },
-  
 ];
 
 const ProjectsSection = () => {
@@ -41,20 +39,21 @@ const ProjectsSection = () => {
       <Heading as="h1" id="projects-section">
         Featured Projects
       </Heading>
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(2,minmax(0,1fr))"
-        gridGap={8}
-      >
-        {projects.map((project) => (
-          <Card
-            key={project.title}
-            title={project.title}
-            description={project.description}
-            imageSrc={project.getImageSrc()}
-            githubLink={project.githubLink}
-          />
-        ))}
+      <Box>
+        <SimpleGrid
+          columns={{ base: 1, md: 2 }}
+          spacing={8}
+        >
+          {projects.map((project) => (
+            <Card
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              imageSrc={project.getImageSrc()}
+              githubLink={project.githubLink}
+            />
+          ))}
+        </SimpleGrid>
       </Box>
     </FullScreenSection>
   );
